a422c802479ff89bb9a58388d8e77d9d43ed5a4a
MURAKI Taichi <muraki.taichi@gmail.com>
2019-10-10 17:46:30 +0900

    fix

--- src/multi-jvm/scala/example/SessionApiSpec.scala
+++ src/multi-jvm/scala/example/SessionApiSpec.scala
@@ -5,6 +5,13 @@ import org.scalatest.WordSpec
 import org.scalatest.MustMatchers
 
 
+import akka.remote.testkit.{ MultiNodeConfig, STMultiNodeSpec }
+
+object MultiNodeSampleConfig extends MultiNodeConfig {
+  val node1 = role("node1")
+  val node2 = role("node2")
+}
+
 class SpecMultiJvmNode1 extends WordSpec with MustMatchers {
   "A node" should {
     "be able to say hello" in {
