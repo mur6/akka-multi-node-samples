f236e990698ee16fb40d4cfe1226aa8130c75805
MURAKI Taichi <muraki.taichi@gmail.com>
2019-10-10 17:56:34 +0900

    fix

--- src/multi-jvm/scala/example/SessionApiSpec.scala
+++ src/multi-jvm/scala/example/SessionApiSpec.scala
@@ -1,15 +1,37 @@
 package example
 
-
 import org.scalatest.WordSpec
 import org.scalatest.MustMatchers
-
-
-import akka.remote.testkit.{ MultiNodeConfig, STMultiNodeSpec }
+import akka.remote.testkit.{MultiNodeConfig, STMultiNodeSpec}
+import com.typesafe.config.ConfigFactory
 
 object MultiNodeSampleConfig extends MultiNodeConfig {
-  val node1 = role("node1")
-  val node2 = role("node2")
+  val seed = role("seed")
+  val master = role("master")
+  val worker1 = role("worker-1")
+  val worker2 = role("worker-2")
+
+  commonConfig(ConfigFactory.parseString(
+    """
+    akka.actor.provider="akka.cluster.ClusterActorRefProvider"
+    """))
+}
+
+// # don't use sigar for tests, native lib not in path
+// akka.cluster.metrics.collector-class = akka.cluster.JmxMetricsCollector
+class WordsClusterSpec extends MultiNodeSpec(MultiNodeSampleConfig)
+  with STMultiNodeSpec with ImplicitSender {
+
+  import WordsClusterSpecConfig._
+
+  def initialParticipants = roles.size
+
+  val seedAddress = node(seed).address
+  val masterAddress = node(master).address
+  val worker1Address = node(worker1).address
+  val worker2Address = node(worker2).address
+
+  muteDeadLetters(classOf[Any])(system)
 }
 
 class SpecMultiJvmNode1 extends WordSpec with MustMatchers {
