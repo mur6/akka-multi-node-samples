b287d6b777f6eac3580305f50a6b7c36d4a6d4e9
MURAKI Taichi <muraki.taichi@gmail.com>
2019-10-10 17:03:57 +0900

    fix

--- src/main/scala/example/TestShard.scala
+++ src/main/scala/example/TestShard.scala
@@ -3,20 +3,20 @@ package example
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings}
 
-object TestShard {
-  private val shardName = "MyShared"
-
-  def startClusterSharding(nrOfShards: Int)(
-    implicit system: ActorSystem
-  ): ActorRef =
-    ClusterSharding(system).start(
-      typeName = shardName,
-      entityProps = Props(new TestActor),
-      settings = ClusterShardingSettings(system),
-      extractEntityId = TestActor.extractEntityId,
-      extractShardId = TestActor.extractShardId(nrOfShards),
-    )
-
-  def shardRegion(implicit system: ActorSystem): ActorRef =
-    ClusterSharding(system).shardRegion(shardName)
-}
+//object TestShard {
+//  private val shardName = "MyShared"
+//
+//  def startClusterSharding(nrOfShards: Int)(
+//    implicit system: ActorSystem
+//  ): ActorRef =
+//    ClusterSharding(system).start(
+//      typeName = shardName,
+//      entityProps = Props(new TestActor),
+//      settings = ClusterShardingSettings(system),
+//      extractEntityId = TestActor.extractEntityId,
+//      extractShardId = TestActor.extractShardId(nrOfShards),
+//    )
+//
+//  def shardRegion(implicit system: ActorSystem): ActorRef =
+//    ClusterSharding(system).shardRegion(shardName)
+//}
--- src/multi-jvm/scala/sample/multinode/MultiNodeSample.scala
+++ src/multi-jvm/scala/sample/multinode/MultiNodeSample.scala
@@ -6,41 +6,41 @@ import akka.testkit.ImplicitSender
 import scala.concurrent.duration._
 import example.TestActor
 
-
-object MultiNodeSampleConfig extends MultiNodeConfig {
-  val node1 = role("node1")
-  val node2 = role("node2")
-}
-
-class MultiNodeSample extends MultiNodeSpec(MultiNodeSampleConfig) with STMultiNodeSpec with ImplicitSender {
-  import MultiNodeSampleConfig._
-
-  def initialParticipants = roles.size
-
-  "A MultiNodeSample" must {
-    "wait for all nodes to enter a barrier" in {
-      enterBarrier("startup")
-    }
-
-    "send to and receive from a remote node" in {
-      runOn(node1) {
-        enterBarrier("deployed")
-        val path = node(node2)
-        println(path)
-        val pActor = system.actorSelection(path / "user" / "test-actor-1")
-        pActor ! TestActor.Command(20)
-        expectMsg(10.seconds, TestActor.Response(21))
-      }
-
-      runOn(node2) {
-        system.actorOf(Props[TestActor], "test-actor-1")
-        enterBarrier("deployed")
-      }
-
-      enterBarrier("finished")
-    }
-  }
-}
-
-class SampleSpecMultiJvmNode1 extends MultiNodeSample
-class SampleSpecMultiJvmNode2 extends MultiNodeSample
+//
+//object MultiNodeSampleConfig extends MultiNodeConfig {
+//  val node1 = role("node1")
+//  val node2 = role("node2")
+//}
+//
+//class MultiNodeSample extends MultiNodeSpec(MultiNodeSampleConfig) with STMultiNodeSpec with ImplicitSender {
+//  import MultiNodeSampleConfig._
+//
+//  override def initialParticipants = roles.size
+//
+//  "A MultiNodeSample" must {
+//    "wait for all nodes to enter a barrier" in {
+//      enterBarrier("startup")
+//    }
+//
+//    "send to and receive from a remote node" in {
+//      runOn(node1) {
+//        enterBarrier("deployed")
+//        val path = node(node2)
+//        println(path)
+//        val pActor = system.actorSelection(path / "user" / "test-actor-1")
+//        pActor ! TestActor.Command(20)
+//        expectMsg(10.seconds, TestActor.Response(21))
+//      }
+//
+//      runOn(node2) {
+//        system.actorOf(Props[TestActor], "test-actor-1")
+//        enterBarrier("deployed")
+//      }
+//
+//      enterBarrier("finished")
+//    }
+//  }
+//}
+//
+//class SampleSpecMultiJvmNode1 extends MultiNodeSample
+//class SampleSpecMultiJvmNode2 extends MultiNodeSample
